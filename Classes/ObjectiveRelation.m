32f119ed95ae65b4067b2c74db3a8e9b255b80b5
Stephen Celis <stephen@stephencelis.com>
2014-03-11 20:39:46 -0700

    Reorganize ActiveRecord/Relation methods
    Organize by:
    
     - Building fetch requests
     - Executing fetch requests
     - Inserting entities
     - Modifying entities
     - Deleting entities
    
    Signed-off-by: Stephen Celis <stephen@stephencelis.com>

--- Classes/ObjectiveRelation.m
+++ Classes/ObjectiveRelation.m
@@ -57,10 +57,16 @@
     return self;
 }
 
+#pragma mark - Fetch request building
+
+- (id)all {
+    return [self copy];
+}
+
 - (id)where:(id)condition, ... {
     va_list arguments;
     va_start(arguments, condition);
-    ObjectiveRelation *relation = [self where:condition arguments:arguments];
+    id relation = [self where:condition arguments:arguments];
     va_end(arguments);
 
     return relation;
@@ -107,29 +113,35 @@
     return relation;
 }
 
+#pragma mark Counting
+
 - (NSUInteger)count {
     return [self.managedObjectContext countForFetchRequest:[self prepareFetchRequest] error:nil];
 }
 
-- (instancetype)all {
-    return [self copy];
+#pragma mark Plucking
+
+- (id)firstObject {
+    return [[[self limit:1] fetchedObjects] firstObject];
 }
 
-- (id)first {
-    return [[self limit:1] firstObject];
-}
-
-- (id)last {
-    return [[self reverseOrder] first];
+- (id)lastObject {
+    return [[self reverseOrder] firstObject];
 }
 
 - (id)find:(id)condition, ... {
     va_list arguments;
     va_start(arguments, condition);
-    ObjectiveRelation *relation = [self where:condition arguments:arguments];
+    id relation = [self where:condition arguments:arguments];
     va_end(arguments);
 
-    return [relation first];
+    return [relation firstObject];
+}
+
+#pragma mark - Manipulating entities
+
+- (id)findOrCreate:(NSDictionary *)properties {
+    return [[self where:properties] firstObject] ?: [self create:properties];
 }
 
 - (id)create {
@@ -145,10 +157,6 @@
     return newEntity;
 }
 
-- (id)findOrCreate:(NSDictionary *)properties {
-    return [[self where:properties] first] ?: [self create:properties];
-}
-
 - (void)updateAll:(NSDictionary *)attributes {
     for (NSManagedObject *entity in self) {
         [entity update:attributes];
@@ -159,13 +167,6 @@
     for (NSManagedObject *entity in self) {
         [entity delete];
     }
-}
-
-- (NSArray *)fetchedObjects {
-    if (_fetchedObjects == nil) {
-        _fetchedObjects = [self.managedObjectContext executeFetchRequest:[self prepareFetchRequest] error:nil];
-    }
-    return _fetchedObjects;
 }
 
 #pragma mark - NSObject
@@ -203,6 +204,13 @@
 }
 
 #pragma mark - Private
+
+- (NSArray *)fetchedObjects {
+    if (_fetchedObjects == nil) {
+        _fetchedObjects = [self.managedObjectContext executeFetchRequest:[self prepareFetchRequest] error:nil];
+    }
+    return _fetchedObjects;
+}
 
 - (NSFetchRequest *)prepareFetchRequest {
     NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:[self.entity entityName]];
